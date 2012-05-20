4d59110b5f9fb5dbb1ab9ae0f5ba2e5b605750fe
Mickaël Floc'hlay <mickael@edupad.com>
2012-05-20 16:42:46 +0200

    Issue #4: basic pagination set up

--- theme/index.php
+++ theme/index.php
@@ -11,4 +11,16 @@
 <?php else : ?>
 <?php endif; ?>
 
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+<?php if ($wp_query->max_num_pages > 1) : ?>
+  <ul class="pager" id="post-nav">
+    <li class="post-previous">
+      <?php next_posts_link( 'plus vieux' ); ?>
+    </li>
+    <li class="post-next">
+      <?php previous_posts_link( 'plus récent' ); ?>
+    </li>
+  </ul>
+<?php endif; ?>
+
 <?php get_footer(); ?>
